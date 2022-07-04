89f545fe31ef79f469975829b4212913c9658c63
Bryan Medina <bryan.medina.per@gmail.com>
2022-07-04 11:21:49 -0500

    Add set grid size, and erase grid fucntions

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 const container = document.querySelector('.container');
+const btn = document.querySelector('button');
+let gridSize = 5;
 
-for(let i = 0; i < 25; i++){
-  let div = document.createElement('div');
-  div.classList.add(`square${i}`);
-  container.appendChild(div);
+function createGrid(gridSize){
+  for(let i = 0; i < gridSize**2; i++){
+    let div = document.createElement('div');
+    div.classList.add(`square${i}`);
+    container.appendChild(div);
+  } 
 }
 
-const divs = document.querySelectorAll('.container>div');
+function eraseGrid(){
+  while(container.firstChild){
+    container.removeChild(container.firstChild);
+  }
+}
 
-divs.forEach((div) => {
-  div.addEventListener('mouseover', (e) => {
-    e.target.style.background = '#b5838d';
-  })
-})
+function drawOnGrid(){
+  const divs = document.querySelectorAll('.container>div');
+ 
+  divs.forEach((div) => {
+    div.addEventListener('mouseover', (e) => {
+      e.target.style.background = '#e5989b';
+    });
+  }); 
+}
+
+btn.addEventListener('click', (e) => {
+  gridSize = parseInt(prompt('Please enter the new grid size', '5'));
+  if(gridSize < 2 || gridSize > 10){
+    alert('The range is between 2 and 10');
+  }
+  else{
+    container.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, auto)`;
+    eraseGrid();
+    createGrid(gridSize);
+    drawOnGrid();    
+  }
+});
+
+createGrid(gridSize);
+drawOnGrid();
+
+
 
 
